e908946a36be168651b4d69786389ee35cc1f918
Rysztun, Sebastian <sebastian.rysztun@gmail.com>
2019-10-24 11:59:16 +0200

    changes in anyval-test example

--- anyval-test/test1/Readme.md
+++ anyval-test/test1/Readme.md
@@ -1,6 +1,8 @@
 Steps:
 
-1. `scalac Test1.scala`
-2. `javap -c UserId`
-3. `javap -c ClassWithKeyAsUserId.class`
-4. `javap -c WrapperOnUserId.class`
+1. `scalac Test1.scala` - compile classes
+2. `javap Test` - disasseble Test object
+3. `javap WrapperOnUserId` - disasseble WrapperOnUserId, there is String no UserId
+4. `javap WrapperOnUserIdentifier.class` - disasseble WrapperOnUserIdentifier, there is UserIdentifier so we're boxing String in our domain class
+5. `javap ClassWithKeyAsUserId.class` - disasseble ClassWithKeyAsUserId, map key is String
+6. `javap ClassWithKeyAsUserIdentifier.class`- disasseble ClassWithKeyAsUserIdentifier, map key is UserIdentifier, again we're boxing String in our domain class
--- anyval-test/test1/Test1.scala
+++ anyval-test/test1/Test1.scala
@@ -1,13 +1,20 @@
 class UserId(val value: String) extends AnyVal
+class UserIdentifier(val value: String)
 
 class ClassWithKeyAsUserId(map: Map[UserId, String])
+class ClassWithKeyAsUserIdentifier(map: Map[UserIdentifier, String])
 
 class WrapperOnUserId(userId: UserId)
+class WrapperOnUserIdentifier(userId: UserIdentifier)
 
 object Test {
 
   val testUserId = new UserId("test")
 
-  val x = new ClassWithKeyAsUserId(Map(testUserId -> "test"))
-  val y = new WrapperOnUserId(testUserId)
+  val testClassWithKeyAsUserId = new ClassWithKeyAsUserId(Map(testUserId -> "test"))
+  val testWrapperOnUserId = new WrapperOnUserId(testUserId)
+
+  val testUserIdentifier = new UserIdentifier("test")
+  val testClassWithKeyAsUserIdentifier = new ClassWithKeyAsUserIdentifier(Map(testUserIdentifier -> "test"))
+  val testWrapperOnUserIdentifier = new WrapperOnUserIdentifier(testUserIdentifier)
 }
