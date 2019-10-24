cdc9484a8ac875c4cc7d2e1b340468c09a49964a
Rysztun, Sebastian <sebastian.rysztun@gmail.com>
2019-10-24 12:02:30 +0200

    changes in anyval-test example

--- anyval-test/test1/Test1.scala
+++ anyval-test/test1/Test1.scala
@@ -1,5 +1,20 @@
 class UserId(val value: String) extends AnyVal
+class UserIdentifier(val value: String)
 
 class ClassWithKeyAsUserId(map: Map[UserId, String])
+class ClassWithKeyAsUserIdentifier(map: Map[UserIdentifier, String])
 
 class WrapperOnUserId(userId: UserId)
+class WrapperOnUserIdentifier(userId: UserIdentifier)
+
+object Test {
+
+  val testUserId = new UserId("test")
+
+  val testClassWithKeyAsUserId = new ClassWithKeyAsUserId(Map(testUserId -> "test"))
+  val testWrapperOnUserId = new WrapperOnUserId(testUserId)
+
+  val testUserIdentifier = new UserIdentifier("test")
+  val testClassWithKeyAsUserIdentifier = new ClassWithKeyAsUserIdentifier(Map(testUserIdentifier -> "test"))
+  val testWrapperOnUserIdentifier = new WrapperOnUserIdentifier(testUserIdentifier)
+}
